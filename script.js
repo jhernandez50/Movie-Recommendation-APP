ba634ebc7cd28d050bcd29811828f407f45b3b7b
Metalistenia <sauceda92zuki@gmail.com>
2021-08-11 21:26:08 -0500

    input query

--- script.js
+++ script.js
@@ -37,3 +37,57 @@ const options = {
 	  console.error(error);
   });
 }
+
+const appendResponse=(data)=>{
+	const ranking = $("#ddn-ranking :selected").val();
+	
+	let markup;
+	if($('#movie-container').has('div').length>0){
+		$('#movie-container').empty();
+	};
+	console.log(sortDataByRanking(data,ranking));
+
+	sortDataByRanking(data,ranking).map(movie=>{
+		const {title,overview,imdbVoteCount,year,imdbRating}=movie;
+		const url = movie.posterURLs[154];
+		markup= buildMarkup(url,title,overview,imdbVoteCount,imdbRating/10,year);
+	$('#movie-container').append(markup);
+	})
+}
+
+const sortDataByRanking=(data,ranking)=>{
+	console.log(ranking)
+	let sortedData=[];
+	data.map(movie=>{
+		console.log(ranking)
+		if(movie.imdbRating/10>=ranking){
+			sortedData.push(movie);
+		}
+	})
+	return sortedData
+}
+
+const buildMarkup=(url,title,overview,votes,rating,year)=>{
+	return `<div class="movie-card">
+	<div class="columns">
+	  <div class="column is-one-quarter">
+		<img style="display: block;" class="mx-auto" src=${url} alt="">
+	  </div>
+	  <div class="column is-four-fifth">
+		<h3 class="sub-heading1 mb-2">${title}</h3>
+		<p class="text-long">${overview}</p>
+		  <div class="columns">
+			<div class="column is-one-fifth">
+				<h3 class="sub-heading2 mt-3">Year: <span>${year}</span></h3>
+			</div>
+			<div class="column is-one-fifth">
+			  <h3 class="sub-heading2 mt-3">Rating: <span>${rating}/10</span></h3>
+			</div>
+			<div class="column is-one-fifth">
+			  <h3 class="sub-heading2 mt-3">Votes: <span>${votes}</span></h3>
+			</div>
+		  </div>
+	  </div>
+	</div>
+  </div>`
+}
